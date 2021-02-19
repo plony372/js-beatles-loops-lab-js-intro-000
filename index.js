4ac98fe957731b611b045468a8949b0881be3346
plony benav <menachemfuterfas@gmail.com>
2021-02-19 16:19:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(musiciansArray, instrumentsArray){
+  var emptyArray = [];
+  for (var i = 0; i < musiciansArray.length; i++){
+    emptyArray.push(`${musiciansArray[i]} plays ${instrumentsArray[i]}`);
+  }
+  return emptyArray;
+}
+
+
+
+function johnLennonFacts(factsArray){
+  var key = 0;
+while(value = factsArray[key++]){
+    console.log(value);
+}
+}
